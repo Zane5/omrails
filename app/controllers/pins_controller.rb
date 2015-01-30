8778613e038d61f9e05c25569478ca5966e1ae98
Zane5 <fat.fox@gmail.com>
2015-01-30 09:30:48 +0000

    Add Pins Associations

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,5 @@
 class PinsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
@@ -13,26 +14,27 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.new
     respond_with(@pin)
   end
 
   def edit
+    @pin = current_user.pins.find(params[:id])
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.new(pin_params)
     @pin.save
     respond_with(@pin)
   end
 
   def update
-    @pin.update(pin_params)
+    @current_user.update(pin_params)
     respond_with(@pin)
   end
 
   def destroy
-    @pin.destroy
+    @current_user.destroy
     respond_with(@pin)
   end
 
